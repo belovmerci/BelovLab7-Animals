8734b0d16bfbda6869bc603a25767c38c85686d9
belovmerci <126976043+belovmerci@users.noreply.github.com>
2023-04-01 10:04:17 +0700

    Add files via upload
    Barbarically changed the file through web browser

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace BelovLab7
 {
+    // Animals
     class Animal
     {
         public string name;
@@ -59,7 +60,9 @@ namespace BelovLab7
         public override void Eat(Food food)
         {
             if (isGoodBoy) Console.WriteLine($"{this.name} is a good boy!");
-            base.Eat(food);
+            if (food is Bone) Console.WriteLine($"{food} is a bone!");
+            if (food is  Hay) Console.WriteLine($"{this.name} refuses to eat hay!");
+            else base.Eat(food);
         }
         public override void Voice()
         {
@@ -84,7 +87,7 @@ namespace BelovLab7
         }
     }
 
-
+    // Foods
     class Food
     {
         public string name;
@@ -98,32 +101,56 @@ namespace BelovLab7
 
     class Fish : Food
     {
-
+        string fishType;
+        public Fish(string name = "Fish", int hungrinessRestore = 10, string fishType = "Koi") : base(name, hungrinessRestore)
+        {
+            this.fishType = fishType;
+        }
     }
     class Bone : Food
     {
-
+        int boneHardness;
+        public Bone(string name = "Bone", int hungrinessRestore = 0, int boneHardness = 10) : base(name, hungrinessRestore)
+        {
+            this.boneHardness = boneHardness;
+        }
     }
 
     class Hay : Food
     {
-
+        int hayFreshness;
+        public Hay(string name = "Fish", int hungrinessRestore = 10, int hayFreshness = 1000) : base(name, hungrinessRestore)
+        {
+            this.hayFreshness = hayFreshness;
+        }
     }
 
 
- 
+    // Controller has animal creation and feeding in it
     class Controller
     {
         static void Feed(Animal animal, Food food)
         {
-            animal.hungriness += food.hungrinessRestore;
+            animal.Eat(food);
         }
 
         public static void RunSim()
         {
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+            Horse horse = new Horse();
 
+            Fish fish = new Fish();
+            Bone bone = new Bone();
+            Hay hay = new Hay();
+
+            Feed(cat, fish);
+            Feed(dog, bone);
+            Feed(horse, hay);
         }
     }
+
+    // Main Program
     class Program
     {
         static void Main(string[] args)
